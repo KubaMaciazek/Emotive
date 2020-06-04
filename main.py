eb427d09aabb072b407fb6385e482fa1121fb70c
malinowska423 <amalinowska423@gmail.com>
2020-06-04 23:18:52 +0200

    basic two screens created

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('emotive.kv')
 
 
-class MainApp(App):
+class MainScreen(Screen):
+    pass
+
+
+class LoadingScreen(Screen):
+    pass
+
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='main'))
+sm.add_widget(LoadingScreen(name='loading'))
+
+
+class Emotive(App):
+
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-
-        return label
+        return sm
 
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+    Emotive().run()
