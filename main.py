e47b55a6ab5ea54ec9d97d4868ae4e5be994acae
malinowska423 <amalinowska423@gmail.com>
2020-06-05 21:03:00 +0200

    quick ui fixes

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.clipboard import Clipboard
+from kivy.clock import Clock
+from kivy.factory import Factory
+from kivy.uix.modalview import ModalView
 
 Builder.load_file('screens/main-screen.kv')
+Builder.load_file('screens/copied-toast.kv')
+
+
+class Toast(ModalView):
+    def __init__(self, **kwargs):
+        super(Toast, self).__init__(**kwargs)
+        Clock.schedule_once(self.dismiss_toast, 1)
+
+    def dismiss_toast(self, dt):
+        self.dismiss()
 
 
 class MainScreen(Screen):
@@ -13,8 +27,8 @@ class MainScreen(Screen):
         self.ids.emoji_output.text = ""
 
     def copy_output(self):
-        # TODO: pop up 'copied to clipboard'
-        self.ids.emoji_output.text = "Copied!"
+        Clipboard.copy(self.ids.emoji_output.text)
+        Factory.Toast().open()
 
 
 class Emotive(App):
