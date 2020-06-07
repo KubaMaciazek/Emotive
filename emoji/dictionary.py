731d533a3159b2bb74952ca68c8c898bb8cda47b
KubaMaciazek <politechnix98@gmail.com>
2020-06-07 13:35:59 +0200

    extend dictionary

--- emoji/dictionary.py
+++ emoji/dictionary.py
@@ -1,3 +1,9 @@
 emoji_dict = {
+    "anger": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
+    "disgust": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
+    "fear": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
     "happy": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
+    "neutral": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
+    "sadness": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
+    "surprise": ["\U0001F600", "data/emoji/emoji_u1f600.png"],
 }
