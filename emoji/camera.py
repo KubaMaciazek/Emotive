f8ee0626c744562c10ab8cc30420e0fbddd08652
malinowska423 <amalinowska423@gmail.com>
2020-06-07 18:29:21 +0200

    slow video render fix

--- emoji/camera.py
+++ emoji/camera.py
@@ -14,11 +14,14 @@ class EmojiRecognitionCamera(Image):
         self.model = cv2.face.FisherFaceRecognizer_create()
         self.model.read("model-30.xml")
         self.pred = 0
-        self.history = self.get_history()
+        self.history = None
         self.current_emotion = emotions[3]
-        Clock.schedule_interval(self.update, 1.0 / fps)
+        self.frame_count = 0
+        self.fps = fps
+        Clock.schedule_interval(self.update, 1.0 / self.fps)
 
     def update(self, dt):
+        self.frame_count = self.frame_count + 1 if self.frame_count < (self.fps/2) else 0
         ret, frame = self.capture.read()
         if ret:
             # convert it to texture
@@ -29,32 +32,9 @@ class EmojiRecognitionCamera(Image):
             # display image from the texture
             self.texture = image_texture
 
-            # get emoji
-            # Update history - remove oldest, add new
-            # _, frame = cap.read()
-            # gray = prepare_photo(frame)
-            # try:
-            #     new_pred, _ = model.predict(gray)
-            #     history.pop(0)
-            #     history.append(new_pred)
-            #     occ = Counter(history)
-            #     pred = occ.most_common(1)[0][0]
-            # except:
-            #     continue
-
-            # Renew history - replace last group with new one
-            gray = prepare_photo(frame)
-            try:
-                new_pred, _ = self.model.predict(gray)
-            except:
-                return
-            if len(self.history) == 10:
-                occ = Counter(self.history)
-                self.pred = occ.most_common(1)[0][0]
-                self.current_emotion = emotions[self.pred]
-                self.history = []
-            else:
-                self.history.append(new_pred)
+            if self.frame_count == 0:
+                self.recognize_emotion(frame)
+                self.update_emoji_preview()
 
     def get_history(self):
         quantity = 10
@@ -69,5 +49,38 @@ class EmojiRecognitionCamera(Image):
                 continue
         return history
 
+    def recognize_emotion(self, frame):
+        if self.history is None:
+            self.history = self.get_history()
+        # get emoji
+        # Update history - remove oldest, add new
+        # _, frame = cap.read()
+        # gray = prepare_photo(frame)
+        # try:
+        #     new_pred, _ = model.predict(gray)
+        #     history.pop(0)
+        #     history.append(new_pred)
+        #     occ = Counter(history)
+        #     pred = occ.most_common(1)[0][0]
+        # except:
+        #     continue
+
+        # Renew history - replace last group with new one
+        gray = prepare_photo(frame)
+        try:
+            new_pred, _ = self.model.predict(gray)
+        except:
+            return
+        if len(self.history) == 10:
+            occ = Counter(self.history)
+            self.pred = occ.most_common(1)[0][0]
+            self.current_emotion = emotions[self.pred]
+            self.history = []
+        else:
+            self.history.append(new_pred)
+
     def get_current_emoji(self):
         return emoji_dict[self.current_emotion]
+
+    def update_emoji_preview(self):
+        return
